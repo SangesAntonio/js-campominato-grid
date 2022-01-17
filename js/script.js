d484b9e4996218e462353df3bc09f86341676ecf
SangesAntonio <sanges65@gmail.com>
2022-01-17 16:32:17 +0100

    added options value

--- js/script.js
+++ js/script.js
@@ -5,13 +5,40 @@ con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.*/
 
+
 //?Funzioni essenziali
 
 
 
-// eleemnti  del DOM
 
+// eleemnti  del DOM
+const button=document.getElementById('bottone');
+const difficulty=document.getElementById('difficult');
 
 
 //***SVOLGIMENTO  */
 
+button.addEventListener('click', function (){
+
+    let cell;
+    let coll;
+    const totalCell= cell * coll;
+    
+    if(difficulty.value == 1){
+        cell = 10
+        coll = 10
+    }else if( difficulty.value == 2){
+        cell = 9;
+        coll = 9;
+        
+    }else{
+        cell = 7;
+        coll = 7;
+    }
+    
+    console.log(cell, coll)
+    
+    
+    
+    
+})
